9e96df7d5fb1531b5e008cecc51e0d5e206c98ca
Antonio Chinea <creative.studio.acts@gmail.com>
2011-12-06 15:39:38 -0500

    Created new Rails project and edited the README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = 'c6a9d80ce8c4819b2ed47393c7bc80f8ddab82d63be80f7cdaaf547ab64e63e87e5e868004ba575b4a85f8dbeb27af0b0a97cfe8665563361fa028db3dc43637'
